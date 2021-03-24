c2030d6fb170312ef0534e49cdbe5a53d51741bb
data07 <data07@DESKTOP-9D836DK>
2021-03-24 09:42:01 +0900

    03/24

---
+++ MhSeoPrj/src/Ex03_15.java
@@ -0,0 +1,23 @@
+
+public class Ex03_15 
+{
+
+	public static void main() 
+	{
+		String str1 = "IT CookBook 입니다.";
+		String str2 = "10";
+		String str3 = "20";
+		
+		str1 = "Java 입니다.";
+		
+		System.out.println("str1 : " + str1);
+		System.out.println("더하기 결과 : " + str2 + str3);
+	
+		int num1 = Integer.parseInt(str2);
+		int num2 = Integer.parseInt(str3);
+		System.out.println("더하기 결과 : " + num1 + num2);
+			
+	}
+	
+	
+}
