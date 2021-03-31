e7e17d46406b7ad84a12ab975fe84527fde98353
data07 <data07@DESKTOP-9D836DK>
2021-03-31 10:12:32 +0900

    03/31

---
+++ MhSeoPrj/src/Ex05_07.java
@@ -0,0 +1,31 @@
+
+public class Ex05_07 
+{
+
+	public static void main(String[] args) 
+	{
+		int a = 75;
+		
+		if (a > 50)
+		{
+			if(a < 100)
+			{
+				System.out.println("50보다 크고 100보다 작군요...");
+			}
+			
+			
+			else
+			{
+				System.out.println("와~ 100 보다 크군요....");
+			}
+			
+		}
+		
+		else
+		{
+			System.out.println("애개~ 50보다 작군요...");
+		}
+		
+	}
+
+}
