f4c2c6438992556b00e3c769fe18cdc6a07ed378
data07 <data07@DESKTOP-9D836DK>
2021-04-07 10:37:55 +0900

    hots

---
+++ MhSeoPrj/src/Ex07_02.java
@@ -0,0 +1,21 @@
+
+public class Ex07_02 
+{
+
+	public static void main(String[] args) 
+	{
+		int hap = 0;
+		int i;
+		
+		i = 0;
+		while(i <= 10)
+		{
+			hap += i;
+			i++;
+		}
+		
+		System.out.println("1에서 10까지의 합 : " + hap);
+		
+	}
+
+}
---
+++ MhSeoPrj/src/Pr_01.java
@@ -0,0 +1,19 @@
+
+public class Pr_01 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		for(int i = 1; i < 101; i++)
+		{
+			if((i % 3 == 0) && (i % 5 == 0))
+			{
+				System.out.println(i);
+			}
+			
+		}
+		
+	}
+
+}
